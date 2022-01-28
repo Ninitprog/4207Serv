26986161bc9d457d6adbe30e53f05feb5cd4e568
Ninitprog <chanrion.nicolas@gmail.com>
2022-01-28 09:53:07 +0100

    formulaire

--- src/Controller/ServeurController.php
+++ src/Controller/ServeurController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class ServeurController extends AbstractController
 {
@@ -17,4 +18,33 @@ class ServeurController extends AbstractController
             'controller_name' => 'ServeurController',
         ]);
 }
+
+    /**
+     * @Route("/serveur/affiche", name="serveur/affiche")
+     */
+    public function affiche(Request $request): Response
+    {
+        $identifiant = $request -> request -> get("identifiant");
+        $password = $request -> request -> get("password");
+        if (($identifiant=="root") && ($password=="toor")){
+            $reponse = "acces autorise";
+         } 
+         else{
+             $reponse = "erreur";
+         }
+        return $this->render('serveur/affiche.html.twig', [
+            'Message' => $reponse,
+        ]);
+}
+
+/**
+     * @Route("/serveur/creerutilisateur", name="serveur")
+     */
+    public function creerutilisateur(): Response
+    {
+        return $this->render('serveur/creerutilisateur.html.twig', [
+            'controller_name' => 'ServeurController',
+        ]);
+}
+
 }
